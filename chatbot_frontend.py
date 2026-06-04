8a20df750364a083ace59092fe20ce75a4f9f52e
Tejaswi <tejaswidevadiga403@gmail.com>
2026-06-04 17:46:27 +0530

    Add sidebar layout for chatbot interface

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,12 +1,22 @@
 import streamlit as st
-from langgraph11_backend import chatbot
+from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
 CONFIG={'configurable':{'thread_id':'threadid-1'}}
 
+#*****************************************Session Setup*****************************************
 if 'message_history' not in st.session_state:
     st.session_state['message_history']=[]
 
+#****************************************Sidebar UI*****************************************
+
+st.sidebar.title("LangGraph Chatbot")
+
+st.sidebar.button("new chat")
+
+st.sidebar.header("My Conversation")
+
+#******************************************Main Ui******************************************
 #to print old messags:-it loops through all the previos conversation and print it on the display
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
